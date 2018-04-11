41f0b7043443d4af3b5b77ad3c9a39552aefcfb3
Monica Seaman <monica.l.seaman@gmail.com>
2018-04-11 17:27:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,6 +60,13 @@ class TicTacToe
     end
   end
 
+  def turn_count(board)
+    board.count { |position| position == "X" || position == "O" }
+  end
+  
+  def current_player(board)
+    turn_count(board).even?? "X" : "O"
+  end
 
 
 end
