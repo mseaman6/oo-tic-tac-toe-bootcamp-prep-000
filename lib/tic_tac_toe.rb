98c0ef97a951c87e12eb931148980ea289cbd76b
Monica Seaman <monica.l.seaman@gmail.com>
2018-04-11 17:07:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,8 +26,8 @@ class TicTacToe
     user_input.to_i - 1
   end  
 
-def move(board, index, char)
-  @board[index] = char
+def move= (index, token = "X")
+  @board[index] = token
   return board
 end
 
