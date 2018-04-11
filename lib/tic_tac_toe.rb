354c19420b9cb54eeefae2f398097433150d35fc
Monica Seaman <monica.l.seaman@gmail.com>
2018-04-11 17:20:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,8 +39,8 @@ class TicTacToe
     end
   end
 
-  def valid_move?(board, index)
-    if index.between?(0, 8) && !position_taken?(board, index)
+  def valid_move?
+    if @index.between?(0, 8) && !position_taken?
       true
     else
       false
