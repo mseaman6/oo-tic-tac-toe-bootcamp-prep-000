e6319a3de25fdf78a2644906d8d6fadf1625bae6
Monica Seaman <monica.l.seaman@gmail.com>
2018-04-11 17:34:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -129,13 +129,13 @@ end
 
 
 
-def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O"
-    true
-  elsif board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  end
-end
+#def position_taken?(board, index)
+#  if board[index] == "X" || board[index] == "O"
+#    true
+#  elsif board[index] == " " || board[index] == "" || board[index] == nil
+#    false
+#  end
+#end
 
 
 
